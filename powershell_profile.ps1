f2427d5808efaf3080742a5be0b4f6702a2683f2
bvanlierop <bartvanlierop@gmail.com>
2019-01-09 19:14:58 +0100

    Added posh-git to profile

--- powershell_profile.ps1
+++ powershell_profile.ps1
@@ -1,7 +1,29 @@
-# Add-PoshGitToProfile -StartSshAgent
-# $Global:GitPromptSettings.BranchBehindAndAheadDisplay = "Compact"
-# Enable-GitColors
+# Tweak UI
+$shell = $Host.UI.RawUI
+$shell.BackgroundColor = "Black"
 
+# Start always at dev
+set-location C:\dev
+
+$shell.ForegroundColor = "White"
+
+# Add alias to notepad++
+new-item alias:np -value "C:\Program Files\Notepad++\notepad++.exe"
+
+# Add alias to Git Extensions
+new-item alias:gitext -value "C:\Program Files (x86)\GitExtensions\GitExtensions.exe"
+
+# Use poshgit
+# Install first via: PowerShellGet\Install-Module posh-git -Scope CurrentUser -AllowPrerelease -Force
+# or Update via: PowerShellGet\Update-Module posh-git
+Enable-GitColors
+$Global:GitPromptSettings.BranchBehindAndAheadDisplay = "Compact"
+Import-Module posh-git
+
+# Disable beep
+Set-PSReadlineOption -BellStyle None
+
+Clear-Host
 # General actions
 function edit ($file) { & "notepad++.exe" $file }
 function vim () { & "${env:ProgramFiles(x86)}\Vim\vim74\gvim.exe" $args }
